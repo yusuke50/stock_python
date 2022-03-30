df32007e2975138ed8047e63ae7872f062923c6b
yusuke50 <yusuke050@gmail.com>
2022-03-30 23:15:57 +0800

    22.03.30 daily v1

---
+++ daily.py
@@ -0,0 +1,121 @@
+from typing import final
+import requests
+import re
+from bs4 import BeautifulSoup
+import yfinance as yf
+from datetime import date, timedelta
+import pandas as pd
+
+""" LIST START """
+todays_list = []
+
+res_FATCA = requests.get("https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZG_DD.djhtm")
+items_FATCA = BeautifulSoup(res_FATCA.text, "html.parser").select("tr .t3t1 a")
+
+for item in items_FATCA:
+    re_inner = re.match(r"^(\w+)", item.decode_contents()[0:6]).group()
+    todays_list.append("{}.TW".format(re_inner))
+
+res_main = requests.get("https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZG_F.djhtm")
+items_main = BeautifulSoup(res_main.text, "html.parser").select("tr .t3t1 a")
+for item in items_main:
+    temp = re.match(r"^(\w+)", item.decode_contents()[0:6]).group()
+
+    for l in todays_list:
+        if (l != temp):
+            todays_list.append("{}.TW".format(temp))
+            break
+
+res_foreign = requests.get(
+    "https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZG_D.djhtm")
+items_foreign = BeautifulSoup(
+    res_foreign.text, "html.parser").select("tr .t3t1 a")
+for item in items_foreign:
+    temp = re.match(r"^(\w+)", item.decode_contents()[0:6]).group()
+
+    for l in todays_list:
+        if (l != temp):
+            todays_list.append("{}.TW".format(temp))
+            break
+
+ori_file = open("stock-list.txt", "r", encoding="utf-8")
+lines = ori_file.read().splitlines()
+ori_file.close()
+
+for item in todays_list:
+    flag = False
+
+    for line in lines:
+        if (line == item):
+            flag = True
+            break
+
+    if (not flag):
+        lines.append(item)
+
+with open("stock-list.txt", "w", encoding="utf-8") as list_file:
+    for line in lines:
+        list_file.write("{}\n".format(line))
+list_file.close()
+""" LIST END """
+
+""" CHECK START """
+today = date.today()
+one_hundred_fifty_day = timedelta(210)
+that_day = today - one_hundred_fifty_day
+
+first_list = open("stock-list.txt", "r", encoding="utf-8")
+first_lines = first_list.read().splitlines()
+first_list.close()
+
+final_list = []
+
+for line in first_lines:
+    flag = True
+
+    tar = yf.Ticker(line)
+    current_price = tar.info["currentPrice"]
+    two_hundred_day_average = tar.info["twoHundredDayAverage"]
+    fifty_day_average = tar.info["fiftyDayAverage"]
+    fifty_two_week_high = tar.info["fiftyTwoWeekHigh"]
+    fifty_two_week_low = tar.info["fiftyTwoWeekLow"]
+
+    historical = tar.history(start=that_day, end=today)
+    df = pd.DataFrame(historical)
+    one_hundred_fifty_day_average = df["Close"].mean()
+
+    last_3_row = df.tail(3)
+    last_5_row = df.tail(5)
+
+    three_day_average = last_3_row["Close"].mean()
+    five_day_average = last_5_row["Close"].mean()
+
+    if current_price <= one_hundred_fifty_day_average:
+        flag = False
+        break
+    elif current_price <= two_hundred_day_average:
+        flag = False
+        break
+    elif one_hundred_fifty_day_average <= two_hundred_day_average:
+        flag = False
+        break
+    elif fifty_day_average <= one_hundred_fifty_day_average:
+        flag = False
+        break
+    elif fifty_day_average <= two_hundred_day_average:
+        flag = False
+        break
+    elif current_price <= fifty_two_week_low * 1.25:
+        flag = False
+        break
+    elif current_price <= fifty_two_week_high * 0.75:
+        flag = False
+        break
+
+    if (flag):
+        final_list.append(line)
+
+with open("final-list.txt", "w", encoding="utf-8") as final_file:
+    for line in final_list:
+        final_file.write("{}\n".format(line))
+final_file.close()
--- yf.py
+++ yf.py
@@ -3,6 +3,7 @@ from datetime import date, timedelta
 import pandas as pd
 
 today = date.today()
+yesterday = today - timedelta(1)
 one_hundred_fifty_day = timedelta(210)
 that_day = today - one_hundred_fifty_day
 
@@ -19,3 +20,22 @@ df = pd.DataFrame(historical)
 one_hundred_fifty_day_average = df["Close"].mean()
 last_3_row = df.tail(3)
 three_day_average = last_3_row["Close"].mean()
+
+data = yf.download("2330.TW", start=yesterday, end=today, interval="1m")
+
+
+def RSI(data, window=14, adjust=False):
+    delta = data['Close'].diff(1).dropna()
+    loss = delta.copy()
+    gains = delta.copy()
+
+    gains[gains < 0] = 0
+    loss[loss > 0] = 0
+
+    gain_ewm = gains.ewm(com=window - 1, adjust=adjust).mean()
+    loss_ewm = abs(loss.ewm(com=window - 1, adjust=adjust).mean())
+
+    RS = gain_ewm / loss_ewm
+    RSI = 100 - 100 / (1 + RS)
+
+    return RSI
