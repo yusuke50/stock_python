bce4e9bd33b42d6f840cd39aa439d7fee1bfacf4
yusuke50 <yusuke050@gmail.com>
2022-04-06 20:04:09 +0800

    22.04.06 update

--- yf.py
+++ yf.py
@@ -1,27 +1,29 @@
 import yfinance as yf
 from datetime import date, timedelta
 import pandas as pd
+import talib
 
-today = date.today()
+
+""" today = date.today()
 yesterday = today - timedelta(1)
 one_hundred_fifty_day = timedelta(210)
 that_day = today - one_hundred_fifty_day
 
 tsmc = yf.Ticker("2330.TW")
-""" current_print = tsmc.info["currentPrice"]  # 收盤
+current_print = tsmc.info["currentPrice"]  # 收盤
 two_hundred_day_average = tsmc.info["twoHundredDayAverage"]  # 200 天
 fifty_day_average = tsmc.info["fiftyDayAverage"]  # 50 天
 fifty_two_week_high = tsmc.info["fiftyTwoWeekHigh"]  # 52週高點
-fifty_two_week_low = tsmc.info["fiftyTwoWeekLow"]  # 52週低點 """
+fifty_two_week_low = tsmc.info["fiftyTwoWeekLow"]  # 52週低點
 
 
 historical = tsmc.history(start=that_day, end=today)
 df = pd.DataFrame(historical)
 one_hundred_fifty_day_average = df["Close"].mean()
 last_3_row = df.tail(3)
-three_day_average = last_3_row["Close"].mean()
+three_day_average = last_3_row["Close"].mean() """
 
-data = yf.download("2330.TW", start=yesterday, end=today, interval="1m")
+data = yf.download("AAPL", start="2022-03-31", end="2022-04-01", interval="1m")
 
 
 def RSI(data, window=14, adjust=False):
