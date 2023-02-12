74e42d102442d898ea6b84d3a7fa07d5a773259d
yusuke050 <yusuke050@gmail.com>
2023-02-13 00:12:18 +0800

    23.02.13 update

--- calc-tw-v2.py
+++ calc-tw-v2.py
@@ -10,22 +10,32 @@ import re
 today = date.today()
 one_hundred_fifty_day = timedelta(210)
 that_day = today - one_hundred_fifty_day
-regex = re.compile(",")
+number_regex = re.compile(",")
 
-with open("temp.txt", "r", encoding="utf-8") as ori_list:
+
+def str_to_number(str):
+    return float(number_regex.sub("", str))
+
+
+with open("stock-list-tw.txt", "r", encoding="utf-8") as ori_list:
     final_list = []
 
     for line in ori_list:
         stock_name = line.rstrip("\n").split(" ")[0]
 
-        if stock_name != "0050.TW" or stock_name != "0056.TW":
-            flagCheck = True
+        etf_flag = True
+        etf_list = ["0050.TW", "0056.TW", "0052.TW"]
+        for etf in etf_list:
+            if stock_name == etf:
+                etf_flag = False
+                break
+
+        if etf_flag:
+            flag_check = True
 
             tar = yf.Ticker(stock_name)
 
             try:
-                # print(stock_name)
-
                 current_price = tar.history(period="1d")["Close"][0]
 
                 session = HTMLSession()
@@ -37,55 +47,60 @@ with open("temp.txt", "r", encoding="utf-8") as ori_list:
                     'section[data-test="qsp-statistics"] > div:nth-child(2) > div:nth-child(2) > div > div tbody tr:nth-child(4) td:nth-child(2)',
                     first=True,
                 ).text
-                fifty_two_week_high = float(regex.sub("", fifty_two_week_high_text))
+                fifty_two_week_high = str_to_number(fifty_two_week_high_text)
 
                 fifty_two_week_low_text = url.html.find(
                     'section[data-test="qsp-statistics"] > div:nth-child(2) > div:nth-child(2) > div > div tbody tr:nth-child(5) td:nth-child(2)',
                     first=True,
                 ).text
-                fifty_two_week_low = float(regex.sub("", fifty_two_week_low_text))
+                fifty_two_week_low = str_to_number(fifty_two_week_low_text)
 
                 fifty_day_average_text = url.html.find(
                     'section[data-test="qsp-statistics"] > div:nth-child(2) > div:nth-child(2) > div > div tbody tr:nth-child(6) td:nth-child(2)',
                     first=True,
                 ).text
-                fifty_day_average = float(regex.sub("", fifty_day_average_text))
+                fifty_day_average = str_to_number(fifty_day_average_text)
 
                 two_hundred_day_average_text = url.html.find(
                     'section[data-test="qsp-statistics"] > div:nth-child(2) > div:nth-child(2) > div > div tbody tr:nth-child(7) td:nth-child(2)',
                     first=True,
                 ).text
-                two_hundred_day_average = float(
-                    regex.sub("", two_hundred_day_average_text)
-                )
+                two_hundred_day_average = str_to_number(two_hundred_day_average_text)
 
                 historical = tar.history(start=that_day, end=today)
                 df = pd.DataFrame(historical)
                 one_hundred_fifty_day_average = df["Close"].mean()
 
                 if current_price < one_hundred_fifty_day_average:
-                    flagCheck = False
+                    flag_check = False
                 elif current_price < two_hundred_day_average:
-                    flagCheck = False
+                    flag_check = False
                 elif one_hundred_fifty_day_average < two_hundred_day_average:
-                    flagCheck = False
+                    flag_check = False
                 elif fifty_day_average < one_hundred_fifty_day_average:
-                    flagCheck = False
+                    flag_check = False
                 elif fifty_day_average < two_hundred_day_average:
-                    flagCheck = False
+                    flag_check = False
                 elif current_price < fifty_two_week_low * 1.25:
-                    flagCheck = False
+                    flag_check = False
                 elif current_price < fifty_two_week_high * 0.75:
-                    flagCheck = False
+                    flag_check = False
             except KeyError as err:
                 print("{} failed ({}).".format(stock_name, err))
-                flagCheck = False
+                flag_check = False
+            except IndexError as err:
+                print("{} failed ({}).".format(stock_name, err))
+                flag_check = False
+            except AttributeError as err:
+                print("{} failed ({}).".format(stock_name, err))
+                flag_check = False
+            except ValueError as err:
+                print("{} failed ({}).".format(stock_name, err))
+                flag_check = False
 
-            if flagCheck:
-                print("{} O".format(stock_name))
+            if flag_check:
+                # print("{} O".format(stock_name))
                 final_list.append(stock_name)
-            # else:
-            # print("{} X".format(stock_name))
 
             time.sleep(1)
 ori_list.close()
