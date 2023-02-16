86ac6c8d3c89066944ab10ae7b17956505854efc
yusuke050 <yusuke050@gmail.com>
2023-02-16 22:34:03 +0800

    23.02.16 "calc-tw-v3"

--- yf-single.py
+++ yf-single.py
@@ -1,24 +1,38 @@
 import yfinance as yf
-from datetime import date
+from datetime import date, timedelta
+import talib
 
 stock_name = "2330.TW"
 tar = yf.Ticker(stock_name)
-print(tar.info)
 
 today = date.today()
+start_day = today - timedelta(60)
 
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup
-
-session = HTMLSession()
-url = session.get(
-    f"https://finance.yahoo.com/quote/{stock_name}/key-statistics?p={stock_name}"
-)
-fifty_two_week_high = url.html.find(
-    'section[data-test="qsp-statistics"] > div:nth-child(2) > div:nth-child(2) > div > div tbody tr:nth-child(4) td:nth-child(2)',
-    first=True,
-)
-print(fifty_two_week_high.text)
+data = yf.download(stock_name, start=start_day, end=today)
+windows_length = 14
 
 
-print(tar.history(period="1d")["Close"][0])
+def RSI(data, window=windows_length, adjust=False):
+    delta = data["Close"].diff(1).dropna()
+    loss = delta.copy()
+    gains = delta.copy()
+
+    gains[gains < 0] = 0
+    loss[loss > 0] = 0
+
+    gain_ewm = gains.ewm(com=window - 1, adjust=adjust).mean()
+    loss_ewm = abs(loss.ewm(com=window - 1, adjust=adjust).mean())
+
+    RS = gain_ewm / loss_ewm
+    RSI = 100 - 100 / (1 + RS)
+
+    return RSI
+
+
+data["RSI"] = talib.RSI(data["Close"], 14)
+print(data["RSI"].iloc[-1])
+
+print("------------------------")
+
+RSI_from_def = RSI(data)
+print(RSI_from_def.iloc[-1])
